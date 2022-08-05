2ab574ed8295bcd6aff8807f37565c22b4930f54
Estelindis <estelindis@gmail.com>
2022-08-05 01:20:59 +0000

    Create initial character and comment models

--- chargen/models.py
+++ chargen/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Character(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    summary = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="char_posts"
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    story = models.TextField(blank=False)
+    notes = models.TextField(blank=True)
+    character_image = CloudinaryField('image', default='placeholder')
+    status = models.IntegerField(choices=STATUS, default=0)
+    library = models.ManyToManyField(
+        User, related_name='story_library', blank=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.name
+
+    def number_of_libraries(self):
+        return self.library.count()
+
+
+class Comment(models.Model):
+    character = models.ForeignKey(Character, on_delete=models.CASCADE,
+                             related_name="comments")
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    body = models.TextField(blank=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
