6cdc596367f78ffb74e7ce586f3fb1a260dc90d2
Estelindis <estelindis@gmail.com>
2022-09-21 16:42:13 +0000

    Add admin helpers incl. filter, search

--- chargen/admin.py
+++ chargen/admin.py
@@ -5,6 +5,9 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Story)
 class StoryAdmin(SummernoteModelAdmin):
+    list_display = ('name', 'slug', 'status', 'created_on')
+    search_fields = ['name', 'summary', 'story', 'notes']
+    list_filter = ('status', 'created_on')
     prepopulated_fields = {'slug': ('name', 'summary')}
     summernote_fields = ('story', 'notes')
 
@@ -14,3 +17,14 @@ class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ['category_name']}
     list_filter = ['category_name']
     list_display = ('category_name', 'category_pic', 'slug')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'story', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
