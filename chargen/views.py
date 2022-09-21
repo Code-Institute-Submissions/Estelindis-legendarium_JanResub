4bb6a056654e92bc0215b402da8e907cfd774c8d
Estelindis <estelindis@gmail.com>
2022-09-21 17:13:32 +0000

    Add StoryList view

--- chargen/views.py
+++ chargen/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse
+from django.views import generic, View
+from django.http import HttpResponseRedirect
+from .models import Story
+from .forms import CommentForm
 
-# Create your views here.
+
+class StoryList(generic.ListView):
+    model = Story
+    queryset = Story.objects.filter(status=1).order_by("-created_on")
+    template_name = "index.html"
+    paginate_by = 6
+
+
